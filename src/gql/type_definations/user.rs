a46097da4cda51bc66e3b68dac96b9f4c5e7f499
pengyihao <pathology666@163.com>
2021-11-18 15:45:34 +0800

    implements a query and now I know how to use sea orm with graphql context and our program starts by just now

--- src/gql/type_definations/user.rs
+++ src/gql/type_definations/user.rs
@@ -1,15 +1,35 @@
-use async_graphql::{Object, Result};
+use async_graphql::{Context, InputObject, Object, Result};
+use sea_orm::{ColumnTrait, Condition, DbConn, EntityTrait, QueryFilter};
 
 #[derive(Default)]
 pub struct UserQuery;
 
-#[Object]
-impl UserQuery {
-    async fn login(&self) -> String{
-        "some fake token".to_string()
-    }
+#[derive(InputObject)]
+pub struct LoginInput {
+    account: String,
+    password: String,
 }
 
+#[Object]
+impl UserQuery {
+    async fn login(&self, ctx: &Context<'_>, input: LoginInput) -> Result<String> {
+        let db = ctx.data_unchecked::<DbConn>();
+        let user = crate::users::Entity::find().filter(
+             Condition::any()
+            .add(crate::users::Column::Username.eq(input.account.clone()))
+            .add(crate::users::Column::Email.eq(input.account.clone()))
+        ).one(db).await.expect("failed to query database");
+        if let Some(user) = user {
+            if user.password == input.password {
+                return Ok("fake token".to_string())
+            } else {
+                return Err(async_graphql::Error::new("bad input: wrong password"))
+            }
+        } else {
+            Err(async_graphql::Error::new("user not found"))
+        }
+    }
+}
 
 #[derive(Default)]
 pub struct UserMutation;
--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,11 @@ use async_graphql::EmptySubscription;
 
 mod gql;
 pub use gql::*;
-
-pub fn build() -> Schema {
-    Schema::new(Query::default(),Mutation::default(), EmptySubscription)
+pub mod entity;
+pub use entity::*;
+use sea_orm::DbConn;
+pub fn build(pool: DbConn) -> Schema {
+    Schema::build(Query::default(),Mutation::default(), EmptySubscription).data(pool).finish()
 }
 
 pub type Schema = async_graphql::Schema<Query, Mutation, EmptySubscription>;
